d8c7266749c39700fd879e2b1195a59626258173
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-03 12:10:15 +0530

    added post route to register new user..

--- api/v1/User/index.js
+++ api/v1/User/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const {registerController} = require('./User.controller');
 
 const app = express.Router();
 
+app.post('/register', registerController);
 
 module.exports = app;
