f9b1f894669fea7c203c4055ba8ab6e129121f88
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-03 11:38:57 +0530

    added User schema.

---
+++ schemas/User.schema.js
@@ -0,0 +1,20 @@
+const Mongoose = require('mongoose');
+
+module.exports = Mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+);
