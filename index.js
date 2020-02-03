6532c4bd03f41cb88825071e357d707a71ca1cd3
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-03 11:04:28 +0530

    init setup for the server index.js file

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+
+const {logger} = require('./config/config');
+const v1Routes = require('./api/v1');
+const errorMiddleware = require('./middlewares/errorMiddleware');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//middlewares
+app.use(require('morgan')('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//mongodb connection
+require('./config/mongoose')();
+
+//API routes
+const apiRoutes = express.Router();
+app.use('/api', apiRoutes);
+v1Routes(apiRoutes);
+
+//error handler
+app.use(errorMiddleware);
+
+app.listen(port, () => logger.info(`Server is running on port - ${port}`));
