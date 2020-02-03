606a2305ff67c03b2d0c98102261a0759d4be1af
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-03 12:09:52 +0530

    registerNewUser model method

--- api/v1/User/User.model.js
+++ api/v1/User/User.model.js
@@ -3,3 +3,7 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User', UserSchema);
 
+exports.registerNewUser = async user => {
+  const newUser = new User({...user});
+  return newUser.save();
+};
