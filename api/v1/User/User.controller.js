f863efbcc71301eed19d51dd3ea7daf0950e985c
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-05 11:06:25 +0530

    added verifyEmailController function.

--- api/v1/User/User.controller.js
+++ api/v1/User/User.controller.js
@@ -1,6 +1,9 @@
 const {encryptPassword} = require('../../../services/password.service');
-const {registerNewUser} = require('./User.model');
-const {errorMessages, logger} = require('../../../config/config');
+const {
+  registerNewUser,
+  verifyEmail,
+} = require('./User.model');
+const {responseMessages, logger} = require('../../../config/config');
 exports.registerController = async (req, res, next) => {
   const {
     name,
@@ -13,9 +16,9 @@ exports.registerController = async (req, res, next) => {
   if (!name || !email || !password) {
     req.error = {
       status: 400,
-      message: errorMessages[400],
+      message: responseMessages[400],
     };
-    return next(new Error());
+    next(new Error());
   }
   try {
     password = await encryptPassword(password);
@@ -28,9 +31,34 @@ exports.registerController = async (req, res, next) => {
     );
     res.status(200).json({
       status: 200,
-      message: `${errorMessages[200]}`,
+      message: `${responseMessages[200]}`,
     });
     logger.info(`[ user.controller.js ] User register successfully with email as ${email}`);
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.verifyEmailController = async (req, res, next) => {
+  try {
+    const {email} = req.query;
+    if (!email) {
+      req.error = {
+        status: 400,
+        message: responseMessages[400],
+      };
+      next(new Error());
+    }
+    const userWithEmail = await verifyEmail(email);
+    res.status(200).json(
+      {
+        status: 200,
+        message: responseMessages[200],
+        data: {
+          isEmailPresent: !!userWithEmail
+        }
+      },
+    );
   } catch (e) {
     next(e);
   }
