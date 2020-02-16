ed5283e9714044769de4f9fae2e016a825a23b72
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-16 16:05:00 +0530

    signIn user API route.

--- api/v1/User/User.controller.js
+++ api/v1/User/User.controller.js
@@ -1,3 +1,6 @@
+const {getToken} = require('../../../services/jwt.service');
+const {comparePassword} = require('../../../services/password.service');
+const {getUserDetails} = require('./User.model');
 const {encryptPassword} = require('../../../services/password.service');
 const {
   registerNewUser,
@@ -21,19 +24,28 @@ exports.registerController = async (req, res, next) => {
     return next(new Error());
   }
   try {
-    password = await encryptPassword(password);
-    await registerNewUser(
-      {
-        name,
-        email,
-        password,
-      },
-    );
-    res.status(200).json({
-      status: 200,
-      message: `${responseMessages[200]}`,
-    });
-    logger.info(`[ user.controller.js ] User register successfully with email as ${email}`);
+    const user = await getUserDetails(email, {email: 1});
+    if (!user) {
+      password = await encryptPassword(password);
+      await registerNewUser(
+        {
+          name,
+          email,
+          password,
+        },
+      );
+      res.status(200).json({
+        status: 200,
+        message: `${responseMessages[200]}`,
+      });
+      logger.info(`[ user.controller.js ] User register successfully with email as ${email}`);
+    } else {
+      req.error = {
+        status: 409,
+        message: responseMessages[409],
+      };
+      next(new Error());
+    }
   } catch (e) {
     next(e);
   }
@@ -47,18 +59,60 @@ exports.verifyEmailController = async (req, res, next) => {
         status: 400,
         message: responseMessages[400],
       };
-     return next(new Error());
+      return next(new Error());
     }
-    const userWithEmail = await verifyEmail(email);
+    const userWithEmail = await getUserDetails(email, {email: 1});
     res.status(200).json(
       {
         status: 200,
         message: responseMessages[200],
         data: {
-          isEmailPresent: !!userWithEmail
-        }
+          isEmailPresent: !!userWithEmail,
+        },
       },
     );
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.signInUserController = async (req, res, next) => {
+  try {
+    const {email, password} = req.query;
+    if (!email || !password) {
+      req.error = {
+        status: 400,
+        message: responseMessages[400],
+      };
+      return next(new Error());
+    }
+    const userDetails = await getUserDetails(email, {email: 1, password: 1, name: 1});
+    if (!userDetails) {
+      req.error = {
+        status: 404,
+        message: responseMessages[404],
+      };
+      return next(new Error());
+    }
+    if (await comparePassword(userDetails.password, password)) {
+      res.status(200).json(
+        {
+          status: 200,
+          message: responseMessages[200],
+          data: {
+            name: userDetails.name,
+            email: userDetails.email,
+            token: getToken({email: userDetails.email}),
+          },
+        },
+      );
+    } else {
+      req.error = {
+        status: '401',
+        message: responseMessages[401],
+      };
+      return next(new Error());
+    }
   } catch (e) {
     next(e);
   }
