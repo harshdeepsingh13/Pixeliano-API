20d6120c1c0fb2212d72f078dc39f7ac5e65985a
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-16 16:04:09 +0530

    added response codes and their messages.

--- api/v1/User/index.js
+++ api/v1/User/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const {
   registerController,
-  verifyEmailController
+  verifyEmailController,
+  signInUserController,
 } = require('./User.controller');
 
 const app = express.Router();
 
 app.post('/register', registerController);
 app.get('/verifyEmail', verifyEmailController);
+app.get('/signIn', signInUserController);
 
 module.exports = app;
