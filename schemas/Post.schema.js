d5df607f75a42cc97cf24fc4fc5d1ade84a491c6
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-16 19:24:26 +0530

    added Posts schema

---
+++ schemas/Post.schema.js
@@ -0,0 +1,29 @@
+const Mongoose = require('mongoose');
+
+module.exports = Mongoose.Schema(
+  {
+    userEmail: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    pictureId: {
+      type: Mongoose.Types.ObjectId,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Mongoose.Types.Array,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+  },
+);
