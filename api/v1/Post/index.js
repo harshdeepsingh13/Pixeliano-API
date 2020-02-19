bb0e7862597c46948f9ccfae33de5d599cc31226
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-02-19 22:25:04 +0530

    added /getAllPosts controller.

--- api/v1/Post/index.js
+++ api/v1/Post/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {
   newPostController,
+  getPostsController
 } = require('./Post.controller');
 
 const app = express.Router();
 
 app.post('/newRecord', newPostController);
+app.get('/getAllPosts', getPostsController);
 
 module.exports = app;
