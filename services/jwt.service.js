96045311032c2617d066752fe9d87cb884e7c318
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-03-19 00:28:59 +0530

    used process.env for public and private keys instead of static files.

--- services/jwt.service.js
+++ services/jwt.service.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const cryptojs = require('crypto-js');
 
-const publicKey = fs.readFileSync(
+/*const publicKey = fs.readFileSync(
 	path.join(__dirname, './keys/public.key'),
 	{
 		encoding: 'utf-8'
@@ -14,13 +14,14 @@ const privateKey = fs.readFileSync(
 	{
 		encoding: 'utf-8'
 	}
-);
+);*/
+
 const jwtConfig = {
-	issuer: "Harshdeep Singh",
-	subject: "harshdeepsingh13@gmail.com",
-	audience: "",
-	expiresIn: "12h",
-	algorithm: "RS256"
+	issuer: 'Harshdeep Singh',
+	subject: 'harshdeepsingh13@gmail.com',
+	audience: '',
+	expiresIn: '12h',
+	algorithm: 'RS256',
 };
 
 const signOptions = {
@@ -39,6 +40,6 @@ const verifyOptions = {
 	algorithm: [jwtConfig.algorithm]
 };
 
-exports.getToken = payload => cryptojs.AES.encrypt(jwt.sign(payload, privateKey, signOptions), privateKey).toString();
+exports.getToken = payload => cryptojs.AES.encrypt(jwt.sign(payload, process.env.PRIVATE_KEY, signOptions), process.env.PRIVATE_KEY).toString();
 
-exports.getPayload = token => jwt.verify(cryptojs.AES.decrypt(token.toString(), privateKey).toString(cryptojs.enc.Utf8), publicKey, verifyOptions);
+exports.getPayload = token => jwt.verify(cryptojs.AES.decrypt(token.toString(), process.env.PRIVATE_KEY).toString(cryptojs.enc.Utf8), process.env.PUBLIC_KEY, verifyOptions);
