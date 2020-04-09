e133d5fc18da28bbb6fed2cb847fc2408929d444
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-04-09 22:22:17 +0530

    implemented functions to save the xml and get the xml.

--- api/v1/User/User.model.js
+++ api/v1/User/User.model.js
@@ -2,10 +2,16 @@ const UserSchema = require('../../../schemas/User.schema');
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User', UserSchema);
+const Rss = mongoose.model('Rss', require('../../../schemas/Rss.schema'));
 
 exports.registerNewUser = async user => {
   const newUser = new User({...user, userId: new mongoose.Types.ObjectId()});
   return newUser.save();
+};
+
+exports.saveXml = async (xmlData) => {
+  const newXml = new Rss({...xmlData, resourceId: new mongoose.Types.ObjectId()});
+  return newXml.save();
 };
 
 /*exports.verifyEmail = email =>
@@ -26,13 +32,44 @@ exports.getUserDetails = (match, projection, matchField = 'email') => {
   if (matchField === 'userId') {
     matchObject.userId = new mongoose.Types.ObjectId(match);
   }
-  return User.findOne(
+  if (projection && projection.xml) {
+    console.log('matchObject', matchObject);
+    return User.aggregate()
+      .match({...matchObject})
+      .lookup({from: 'rsses', localField: 'xmlId', foreignField: 'resourceId', as: 'xml'})
+      .project({_id: 0, ...projection});
+  } else {
+    return User.findOne(
+      {
+        ...matchObject,
+      },
+      {
+        _id: 0,
+        ...projection,
+      },
+    );
+  }
+};
+
+exports.updateXmlId = (match, xmlId, matchField = 'email') => {
+  const matchObject = {};
+  if (matchField === 'email') {
+    matchObject.email = match;
+  }
+  if (matchField === 'userId') {
+    matchObject.userId = new mongoose.Types.ObjectId(match);
+  }
+
+  return User.findOneAndUpdate(
     {
       ...matchObject,
     },
     {
-      _id: 0,
-      ...projection,
+      xmlId
+    },
+    {
+      new: true,
+      useFindAndModify: false,
     },
   );
 };
