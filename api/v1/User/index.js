cb07e713eb71be30fac12c5a4d800919fc03fd50
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-05-06 22:39:44 +0530

    Added API 'PUT /user/saveDefaultTags'

--- api/v1/User/index.js
+++ api/v1/User/index.js
@@ -3,11 +3,15 @@ const {
   registerController,
   verifyEmailController,
   signInUserController,
+  saveDefaultTagsController
 } = require('./User.controller');
+const authenticationMiddleware = require('../../../middlewares/authenticationMiddleware');
+
 
 const app = express.Router();
 
 app.post('/register', registerController);
+app.put('/saveDefaultTags', authenticationMiddleware, saveDefaultTagsController);
 app.get('/verifyEmail', verifyEmailController);
 app.get('/signIn', signInUserController);
 
