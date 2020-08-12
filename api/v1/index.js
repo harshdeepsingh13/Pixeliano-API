8df7beb05868e1c9f026edcc4dc9e763118dd245
Harshdeep Singh <harshdeep.singh@tothenew.com>
2020-08-12 09:52:43 +0530

    moved tag APIs and models out from Post APIs.

--- api/v1/index.js
+++ api/v1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const user = require('./User');
 const post = require('./Post');
 const listing = require('./Listing');
+const tag = require('./Tag');
 const authenticationMiddleware = require('../../middlewares/authenticationMiddleware');
 
 module.exports = v1Routes => {
@@ -9,5 +10,6 @@ module.exports = v1Routes => {
   v1Routes.use('/v1', app);
   app.use('/user', user);
   app.use('/post', authenticationMiddleware, post);
+  app.use('/tag', authenticationMiddleware, tag);
   app.use('/listing', listing);
 };
